e0c5bf09369a35f615205f8d8b5f6f3959458d56
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-02-04 11:27:05 -0600

    first commit

---
+++ src/test/java/com/codeup/halfguard/HalfGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.halfguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalfGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
