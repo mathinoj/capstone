8220d5a30007ea1d9b7d86cb42bae7ff55d4e629
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-02-07 12:29:15 -0600

    added to post controller

--- src/main/java/com/codeup/halfguard/controller/PostController.java
+++ src/main/java/com/codeup/halfguard/controller/PostController.java
@@ -1,22 +1,25 @@
 package com.codeup.halfguard.controller;
 
 import com.codeup.halfguard.models.Post;
+import com.codeup.halfguard.models.User;
 import com.codeup.halfguard.repositories.PostRepository;
 import com.codeup.halfguard.repositories.UserRepository;
+import com.codeup.halfguard.services.EmailService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostController {
     private final PostRepository postDao;
     private final UserRepository userDao;
-//    private final EmailService emailService;
+    private final EmailService emailService;
 
-    public PostController(PostRepository postDao, UserRepository userDao){
+    public PostController(PostRepository postDao, UserRepository userDao, EmailService emailService){
         this.postDao = postDao;
         this.userDao = userDao;
+        this.emailService = emailService;
     }
 
     @GetMapping("/posts")
@@ -26,6 +29,15 @@ public class PostController {
         return "posts/index";
     }
 
+    @GetMapping("/posts/{id}")
+    public String singlePost(@PathVariable long id, Model model){
+        Post singlePost = postDao.getById(id);
+        model.addAttribute("singlePost", singlePost);
+
+        return "posts/display";
+    }
+
+
     @GetMapping("/posts/edit/{id}")
     public String editPost(@PathVariable long id, Model model){
         Post editPost = postDao.getById(id);
@@ -33,7 +45,50 @@ public class PostController {
         model.addAttribute("postToEdit", editPost);
 
         return "/posts/edit";
+    }
+
+    @PostMapping("/posts/edit")
+    public String saveEdit(@RequestParam(name="postTitle") String postTitle, @RequestParam(name="postBody") String postBody, @RequestParam(name="postId") long id){
+        Post postToEdit = postDao.getById(id);
+
+        postToEdit.setTitle(postTitle);
+        postToEdit.setBody(postBody);
+
+        postDao.save(postToEdit);
+
+        return "redirect:/posts";
+    }
 
 
+    @PostMapping("/posts/delete/{id}")
+    public String deletPost(@PathVariable long id){
+        long deletePostId = id;
+        postDao.deleteById(deletePostId);
+
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/posts/create")
+    public String displayCreatePost(Model model){
+        model.addAttribute("post", new Post());
+
+        return "posts/create";
+    }
+
+    @PostMapping("/posts/create")
+    public String createPost(@ModelAttribute Post post){
+        User postCreator = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        post.setUser(userDao.getById(2L));
+        post.setUser(postCreator);
+
+        String emailSubject = post.getUser().getUsername() + ", post has been created!";
+
+        String emailBody = "Congrats your latest post is up: " + post.getTitle() + post.getBody();
+
+        emailService.preparAndSend(post, emailSubject, emailBody);
+        postDao.save(post);
+
+        return "redirect:/posts";
     }
 }
---
+++ src/main/java/com/codeup/halfguard/services/EmailService.java
@@ -0,0 +1,33 @@
+package com.codeup.halfguard.services;
+
+import com.codeup.halfguard.models.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service("mailService")
+public class EmailService {
+    @Autowired
+    public JavaMailSender emailSender;
+
+    @Value("admin@example.com")
+    private String from;
+
+    public void prepareAndSend(Post post, String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(post.getUser().getUsername());
+        message.setSubject(subject);
+        message.setText(body);
+
+        try{
+            this.emailSender.send(message);
+        }
+        catch (MailException exception){
+            System.err.println(exception.getMessage());
+        }
+    }
+}
